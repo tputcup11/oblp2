30d86846e03e9ea9e041f12f18e1c4a6e5cfa6aa
tputcup11 <n0izx11@gmail.com>
2020-05-12 23:21:18 -0300

    Clase Sistema

--- src/juegoSumas/Sistema.java
+++ src/juegoSumas/Sistema.java
@@ -1,5 +1,34 @@
 package juegoSumas;
 
+import java.util.*;
+
 public class Sistema {
+    private ArrayList<Jugador> listaJugadores;
     
+    //Constructor
+    public Sistema(){
+        listaJugadores = new ArrayList<>();
+    }
+    
+    //GetLista
+    public ArrayList<Jugador> getJugadores(){
+        return listaJugadores;
+    }
+    
+    //SetLista
+    public void agregarJugador(Jugador unJugador){
+        listaJugadores.add(unJugador);
+    }                                                                                                                   
+    
+    //Para TESTEAR: Metodo que ordena ArrayList por cant. de partidas ganadas.
+    public ArrayList<Jugador> ordenarGanadores(){
+        Collections.sort(this.getJugadores(), new CriterioGanadas());
+        return this.getJugadores();
+    }
+    //Criterio de comparacion del sort.
+     private class CriterioGanadas implements Comparator<Jugador> {
+        public int compare(Jugador jugador1, Jugador jugador2){
+            return jugador1.getPartidasGanadas() - jugador2.getPartidasGanadas();
+        }
+    }
 }
